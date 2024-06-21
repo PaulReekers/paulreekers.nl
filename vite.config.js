0d4a21ded53c66f08f114943105df69990e96be4
Paul Reekers <paulreekers@gmail.com>
2024-06-21 18:44:25 +0200

    fixed the link issue (I hope)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
-  base: "./", // Set the base path for your project
+  //base: "./", // Set the base path for your project
   root: ".",
   plugins: [WindiCSS()],
   server: {
