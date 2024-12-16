723b706fa068d9fc95bcc127a18602f1b79ccf73
Paul Reekers <paulreekers@gmail.com>
2024-12-16 13:45:13 +0100

    fixed styling issue for bigger screens. Made the inspiration section with Vue

--- src/components/Inspiration.vue
+++ src/components/Inspiration.vue
@@ -2,20 +2,11 @@
   <section class="inspiration full" id="inspiration">
     <h2>Inspiration</h2>
     <ul class="stuffs xx-large">
-      <li class="stuff website">
-        <h3>Add of the world</h3>
+      <li class="stuff website" ref="websites" v-for="(item, index) in items" :key="index">
+        <h3>{{ item.title }}</h3>
         <div>
-          <p>Highlighted Campaigns - Ads of the World</p>
-          <p>Ads of the World (AOTW for short) is part of the Clio Network of sites that celebrates creativity, inspires a competitive marketplace of ideas, and fosters meaningful connections within the creative community. On AOTW we feature an ever-growing advertising campaign database, a customizable watchlist, bookmarks, and personal profiles so you can find the work that inspires you.</p>
-          <p><a href="https://www.adsoftheworld.com/" target="_blank">adsoftheworld.com</a></p>
-        </div>
-      </li>
-      <li class="stuff website">
-        <h3>Fonts In Use</h3>
-        <div>
-          <p>An independent archive of typography.</p>
-          <p>Fonts In Use is a public archive of typography indexed by typeface, format, industry, and period. Supported by examples contributed by the public, we document and examine graphic design with the goal of improving typographic literacy and appreciation. Designers use our site for project research, type selection and pairing, and discovering new ways to choose and use fonts.</p>
-          <p><a href="https://fontsinuse.com/" target="_blank">fontsinuse.com</a></p>
+          <p v-html="item.description"></p>
+          <p><a :href="item.url" target="_blank">{{ item.url }}</a></p>
         </div>
       </li>
     </ul>
@@ -24,6 +15,35 @@
 
 <script>
 export default {
-  name: 'Inspiration'
+  name: 'Inspiration',
+  data() {
+    return {
+      items: [
+        {
+          title: 'Add of the world',
+          description: 'Highlighted Campaigns <br><br> Ads of the World Ads of the World (AOTW for short) is part of the Clio Network of sites that celebrates creativity, inspires a competitive marketplace of ideas, and fosters meaningful connections within the creative community. <br>On AOTW we feature an ever-growing advertising campaign database, a customizable watchlist, bookmarks, and personal profiles so you can find the work that inspires you.',
+          url: 'https://www.adsoftheworld.com/'
+        },
+        {
+          title: 'Fonts In Use',
+          description: 'An independent archive of typography <br><br>Fonts In Use is a public archive of typography indexed by typeface, format, industry, and period. Supported by examples contributed by the public, we document and examine graphic design with the goal of improving typographic literacy and appreciation. Designers use our site for project research, type selection and pairing, and discovering new ways to choose and use fonts.',
+          url: 'https://fontsinuse.com/'
+        },
+        {
+          title: 'Fonts In Use',
+          description: 'An independent archive of typography <br><br>Fonts In Use is a public archive of typography indexed by typeface, format, industry, and period. Supported by examples contributed by the public, we document and examine graphic design with the goal of improving typographic literacy and appreciation. Designers use our site for project research, type selection and pairing, and discovering new ways to choose and use fonts.',
+          url: 'https://fontsinuse.com/'
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.$refs.websites.forEach((website, index) => {
+      website.addEventListener('click', () => {
+        const url = this.items[index].url;
+        window.open(url, '_blank');
+      });
+    });
+  }
 }
 </script>
--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="work full" id="playground">
-    <h2>Playground & Creative Experiments</h2>
+    <h2>Playground</h2>
     <ul class="stuffs xx-large">
+      <li class="stuff project">
+        <h3>Hover Effect with svg image</h3>
+        <div>
+          <p>A recreation of an insane hover effect.</p>
+          <p><a href="/src/playground/hovereffect/" target="_self">Show me</a></p>
+        </div>
+      </li>
       <li class="stuff project">
         <h3>Hover Effect with svg image</h3>
         <div>
